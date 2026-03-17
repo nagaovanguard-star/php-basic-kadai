bf29980727db849ce6a82e49f0c0d3443436badb
nagasihige0210 <nagao.vanguard@gmail.com>
2026-03-18 08:21:06 +0900

    Add files via upload

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html lang="ja">
+
+<head>
+  <meta charset="utf-8">
+  <title>kadai_007</title>
+</head>
+
+<body>
+  <p>
+    <?php
+
+    $personal_data=['name'=>'onion','price'=>200,'wight'=>'160'];
+
+
+    print_r($personal_data);
+
+
+    echo '<br>';
